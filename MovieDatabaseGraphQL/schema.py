780a4031e963877165b5b27b6ccd396afa3f8b73
JoaquinChartier <48659409+JoaquinChartier@users.noreply.github.com>
2021-07-06 22:00:49 -0300

    Added optional parameters to querys

--- MovieDatabaseGraphQL/schema.py
+++ MovieDatabaseGraphQL/schema.py
@@ -22,24 +22,59 @@ class Query(graphene.ObjectType):
     all_movies = graphene.List(MovieType)
     all_actors = graphene.List(ActorType)
     all_characters = graphene.List(CharacterType)
-    movie_by = graphene.List(MovieType, title=graphene.String(required=True), genre=graphene.String(required=True))
+
+    movies_by = graphene.List(MovieType, 
+    title=graphene.String(required=True), 
+    genre=graphene.String(default_value=""), 
+    MPAA_rating=graphene.String(default_value=""),
+    orderByAsc=graphene.String(default_value="id"), 
+    orderByDes=graphene.String(default_value="id"))
+
+    actors_by = all_actors = graphene.List(ActorType, 
+    name=graphene.String(required=True),
+    gender=graphene.String(default_value=""),
+    ethnicity=graphene.String(default_value=""),
+    orderByAsc=graphene.String(default_value="id"), 
+    orderByDes=graphene.String(default_value="id"))
 
     def resolve_all_movies(root, info):
-        # We can easily optimize query count in the resolve method
         return Movie.objects.all()
 
     def resolve_all_actors(root, info):
-        # We can easily optimize query count in the resolve method
         return Actor.objects.all()
 
     def resolve_all_characters(root, info):
-        # We can easily optimize query count in the resolve method
         return Character.objects.all()
 
-    def resolve_movie_by(root, info, title, genre):
+    def resolve_movies_by(root, info, title, genre, MPAA_rating, orderByAsc, orderByDes):
         try:
-            return Movie.objects.filter(title__icontains=title, genre__icontains=genre)
+            orderByAsc = orderByAsc.lower()
+            orderByDes = orderByDes.lower()
+            if (orderByAsc == orderByDes):
+                orderBy = orderByAsc
+            elif (orderByAsc == 'id'):
+                orderBy = f'-{orderByDes}'
+            else:
+                orderBy = orderByAsc
+
+            return Movie.objects.filter(title__icontains=title, genre__icontains=genre, MPAA_rating__icontains=MPAA_rating).order_by(orderBy)
         except Movie.DoesNotExist:
             return None
+
+    def resolve_actors_by(root, info, name, gender, ethnicity, orderByAsc, orderByDes):
+        try:
+            orderByAsc = orderByAsc.lower()
+            orderByDes = orderByDes.lower()
+            if (orderByAsc == orderByDes):
+                orderBy = orderByAsc
+            elif (orderByAsc == 'id'):
+                orderBy = f'-{orderByDes}'
+            else:
+                orderBy = orderByAsc
+
+            return Actor.objects.filter(name__icontains=name, gender__icontains=gender, ethnicity__icontains=ethnicity).order_by(orderBy)
+        except Actor.DoesNotExist:
+            return None
+
 
 schema = graphene.Schema(query=Query)
